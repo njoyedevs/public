de5e123f946d2caae859a7c3f50e4396f5025bd9
Nicolas Joye <nicolasajoye@gmail.com>
2023-01-30 13:44:18 -0700

    good to go

---
+++ Web_Fundamentals_Wks_1-2/Button_clicker_javascript.js
@@ -0,0 +1,11 @@
+function chanceText(element) {
+    element.innerText = "Logout";
+};
+
+function removeElement(element) {
+    element.remove();
+};
+
+function likedAlert() {
+    alert("Ninja was liked!");
+  }
