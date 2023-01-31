b7733fb21eb2fe3a539766c884609c93494acf7c
Nicolas Joye <nicolasajoye@gmail.com>
2023-01-31 12:09:49 -0700

    good to go

--- Web_Fundamentals_Wks_1-2/Likes Page/Likes_javascript.js
+++ Web_Fundamentals_Wks_1-2/Likes Page/Likes_javascript.js
@@ -1,7 +1,21 @@
-var clickAmount = 0;
+var clickAmount_neil = 0;
+var clickAmount_nichole = 0;
+var clickAmount_jim = 0;
 
-function increase() {
-    var like_button = document.querySelector("#like_count")
-    clickAmount++;
-    like_button.innerText = clickAmount + " likes(s)";
+function increase_neil(id) {
+    var like_button = document.querySelector("#" + id)
+    clickAmount_neil++;
+    like_button.innerText = clickAmount_neil + " likes(s)";
+}
+
+function increase_nichole(id) {
+    var like_button = document.querySelector("#" + id)
+    clickAmount_nichole++;
+    like_button.innerText = clickAmount_nichole + " likes(s)";
+}
+
+function increase_jim(id) {
+    var like_button = document.querySelector("#" + id)
+    clickAmount_jim++;
+    like_button.innerText = clickAmount_jim + " likes(s)";
 }
