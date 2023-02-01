836686d92c988b3374e8d946c49af0d638b26576
Nicolas Joye <nicolasajoye@gmail.com>
2023-01-31 18:43:43 -0700

    good to go

--- Web_Fundamentals_Wks_1-2/Dojo Weather/dojoweather_javascript.js
+++ Web_Fundamentals_Wks_1-2/Dojo Weather/dojoweather_javascript.js
@@ -1,50 +1,31 @@
 function remove_cookie_mes() {
-    var element_to_remove = document.getElementById("#remove_cookie");
+    var element_to_remove = document.getElementById("remove_cookie");
     element_to_remove.remove();
 };
 
+function conversion_fah(temp) {
+    var conversion = ((temp * 9/5) + 32).toPrecision(2)
+    return conversion;
+}
+
+function conversion_cel(temp) {
+    var conversion = ((temp - 32) * 5/9).toPrecision(2)
+    return conversion;
+}
+
 function changeTemp() {
 
-    function converstion(temp) {
-        var conversion = (temp * 9/5) + 32
-        return conversion;
+    var temp_change = document.getElementById("select_temps");
+
+    var temps_array = document.querySelectorAll(".temp_low, .temp_high");
+
+    for (var temp of temps_array) {
+        if (temp_change.value == "Fahrenheit") {
+            temp.innerText = conversion_fah(temp.innerText)
+        } else if (temp_change.value == "Celsius") {
+            temp.innerText = conversion_cel(temp.innerText)
+        }
     }
 
-    var temp_change = document.getElementById("#select_temps");
-
-    var r = document.querySelector(':root');
-
-    var rs = getComputedStyle(r);
-
-    root_temp1 = rs.getPropertyValue("--temp1");
-    root_temp2 = rs.getPropertyValue("--temp2");
-    root_temp3 = rs.getPropertyValue("--temp3");
-    root_temp4 = rs.getPropertyValue("--temp4");
-    root_temp5 = rs.getPropertyValue("--temp5");
-    root_temp6 = rs.getPropertyValue("--temp6");
-    root_temp7 = rs.getPropertyValue("--temp7");
-    root_temp8 = rs.getPropertyValue("--temp8");
-
-    if (temp_change == "F") {
-        temp1.innerText = conversion(root_temp1) + "°";
-        temp2.innerText = conversion(root_temp2) + "°";
-        temp3.innerText = conversion(root_temp3) + "°";
-        temp4.innerText = conversion(root_temp4) + "°";
-        temp5.innerText = conversion(root_temp5) + "°";
-        temp6.innerText = conversion(root_temp6) + "°";
-        temp7.innerText = conversion(root_temp7) + "°";
-        temp8.innerText = conversion(root_temp8) + "°";
-    };
-
-    if (temp_change == "C") {
-        temp1.innerText = root_temp1 + "°";
-        temp2.innerText = root_temp2 + "°";
-        temp3.innerText = root_temp3 + "°";
-        temp4.innerText = root_temp4 + "°";
-        temp5.innerText = root_temp5 + "°";
-        temp6.innerText = root_temp6 + "°";
-        temp7.innerText = root_temp7 + "°";
-        temp8.innerText = root_temp8 + "°";
-    };
-    
+    alert("Loading weather report...")
 }
