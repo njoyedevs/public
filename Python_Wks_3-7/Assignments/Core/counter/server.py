cc7293774535d0b7659a898f6c30325156f45b7e
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-10 15:55:19 -0700

    good to go

---
+++ Python_Wks_3-7/Assignments/Core/counter/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, redirect, session # added request and redirect
+
+app = Flask(__name__)
+
+app.secret_key = "safasodhfosidhjoi2fwefj2o3rjf2ofi3j-09"
+
+@app.route('/')
+def index():
+    session["counter"] = 1
+    return render_template("index.html", title="", counter=session["counter"])
+
+@app.route("/click", methods=["POST"])
+def click():
+    return redirect("/counter")
+
+@app.route("/reset", methods=["POST"])
+def reset():
+    return redirect("/destroy_session")
+
+@app.route('/counter')
+def counter():
+    session["counter"] += 1
+    return render_template("index.html", title="counter", counter=session["counter"])
+
+@app.route('/destroy_session')
+def destroy_session():
+    session.clear()
+    return redirect("/")
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
