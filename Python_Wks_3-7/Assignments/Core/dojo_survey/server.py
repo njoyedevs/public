0bcbd7b98369b38247e47f6a4f48dcbc3bbd636f
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-12 22:53:31 -0700

    good to go

--- Python_Wks_3-7/Assignments/Core/dojo_survey/server.py
+++ Python_Wks_3-7/Assignments/Core/dojo_survey/server.py
@@ -6,13 +6,18 @@ app.secret_key = "safasl'ksdjf;oilsdajf;sdloifjj-09"
 
 @app.route('/')
 def index():
-    session["name"] = request.form["name"]
-    session["location"] = request.form["location"]
-    session["fav_lang"] = request.form["fav_lang"]
-    session["comments"] = request.form["comments"]
     session["key"] = "result"
-    print(request.form)
-    return render_template("index.html", name=session["name"], location=session["location"], fav_lang=session["fav_lang"], comment=session["comments"]) 
+    # session["name"] = request.form["name"]
+    # session["location"] = request.form["location"]
+    # session["fav_lang"] = request.form["fav_lang"]
+    # session["comment"] = request.form["comment"]
+    session["name"] = "Nicolas"
+    session["location"] = "San Jose"
+    session["fav_lang"] = "Python"
+    session["comment"] = "Testing Comments"
+    print(f"This is #2 request.form: {request.form}")
+    return render_template("index.html", name=session["name"], location=session["location"], fav_lang=session["fav_lang"], comment=session["comment"]) 
+    # return render_template("index.html")
 
 @app.route("/click", methods=["POST"])
 def click():
@@ -24,14 +29,14 @@ def process():
     if session["key"] == "result":
         addy = "result"
     elif session["key"] == "index":
-        addy = "index"
-    print(request.form)
+        addy = ""
+    print(f"This is #2 request.form: {request.form}")
     return redirect(f"/{addy}")
 
 @app.route('/result')
 def result():
     session["key"] = "index"
-    return render_template("result.html", name=session["name"], location=session["location"], fav_lang=session["favorite_lang"], comment=session["Comment"])
+    return render_template("result.html", name=session["name"], location=session["location"], fav_lang=session["fav_lang"], comment=session["comment"])
 
 if __name__=="__main__":
     app.run(debug=True)
