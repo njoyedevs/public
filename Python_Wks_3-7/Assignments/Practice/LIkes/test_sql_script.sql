97b7b094aa5d51e1bfa491a207e2546d7021ba58
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-14 21:24:50 -0700

    good to go

---
+++ Python_Wks_3-7/Assignments/Practice/LIkes/test_sql_script.sql
@@ -0,0 +1,7 @@
+-- INSERT INTO users (first_name, last_name, email) VALUES('Nicolas', 'Joye', 'njoyehappiness@gmail.com');
+
+SELECT * from users ;
+
+-- SHOW GLOBAL VARIABLES LIKE 'PORT';
+
+-- show variables where variable_name in ('hostname','port');
