d915721276ea99b4e3be45f8db99108f8b4f81c9
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-15 15:28:58 -0700

    updated and added

--- Python_Wks_3-7/Assignments/Flask_MySQL/first_flask_mysql/server.py
+++ Python_Wks_3-7/Assignments/Flask_MySQL/first_flask_mysql/server.py
@@ -1,1 +1,12 @@
-print('Test')
+# Import app variable from the __init__.py via flask_app folder
+from flask_app import app
+
+# Import the controllers via flask_app.controllers folders
+from flask_app.controllers import users_controller
+
+# Create if statement that will verify the script running is the "__main__" script
+if __name__ == "__main__":
+    
+    # use the imported app variable holding the Flask instance to call run function 
+    # with debugging turned on.
+    app.run(debug=True)
