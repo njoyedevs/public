dd6363fd88dd4cbc2b3d17ad6074db41bf00a9e4
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-15 17:26:26 -0700

    good to go.

--- Python_Wks_3-7/Assignments/Flask_MySQL/working_on/flask_app/controllers/friends_controller.py
+++ Python_Wks_3-7/Assignments/Flask_MySQL/working_on/flask_app/controllers/friends_controller.py
@@ -1,18 +1,13 @@
+# Import render_template from flask 
 from flask import render_template
 
-# import the class from friend.py
+# import the Friend class from friend.py to generate Friend objects
 from flask_app.models.friend import Friend
 
-# Import app variable server.py
+# Import app variable server.py to generate instance of Flask
 from flask_app import app
 
-# @app.route("/")
-# def index():
-#     # call the get all classmethod to get all friends
-#     friends = Friend.get_all()
-#     print(friends)
-#     return render_template("index.html")
-
+# Create root route that will get all data from Friend's class and render in template
 @app.route('/')
 def index():
     friends = Friend.get_all()
--- Python_Wks_3-7/Assignments/Flask_MySQL/working_on/server.py
+++ Python_Wks_3-7/Assignments/Flask_MySQL/working_on/server.py
@@ -4,6 +4,7 @@ from flask_app import app
 # Import the controller
 from flask_app.controllers import friends_controller
 
+# Verfifies that __name__ is the main file and run's the app in Debug mode
 if __name__ == "__main__":
     app.run(debug=True)
 
