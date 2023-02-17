7fe2299bab9da49584a938e5f41238012404d1f7
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-17 14:40:21 -0700

    good to go

---
+++ Python_Wks_3-7/Assignments/Practice/Email Validation with DB/flask_app/controllers/email_controller.py
@@ -0,0 +1,24 @@
+from flask import render_template, redirect, request
+
+from flask_app.models.emails import User
+
+from flask_app import app
+
+@app.route('/')
+def index():
+    return render_template("index.html") 
+
+@app.route('/process', methods=["POST"])
+def process():
+    print(request.form)
+    if not User.validate_user(request.form):
+
+        return redirect('/')
+
+    User.save(request.form)
+    
+    return redirect("/success")
+
+@app.route('/success')
+def results():
+    return render_template("success.html", emails=User.get_recent())
---
+++ Python_Wks_3-7/Assignments/Practice/Email Validation with DB/flask_app/models/emails.py
@@ -0,0 +1,48 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+from flask_app import DATABASE
+
+from flask import flash
+
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+class User:
+    def __init__(self,data):
+        self.id = data['id']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        print("initiated")
+        
+    @classmethod
+    def save(cls,data):
+        query = """INSERT INTO emails (email, created_at, updated_at) Values
+                (%(email)s, NOW(), NOW());"""
+        
+        print("Saved")   
+        return connectToMySQL(DATABASE).query_db(query,data)
+    
+    def get_all(cls):
+        query = 'SELECT email, created_at FROM emails;'
+        
+        results = connectToMySQL(DATABASE).query_db(query)
+        # print(results)
+        dojos = []
+        
+        for dojo in results:
+            dojos.append(dojo)
+            
+        return dojos
+    
+    @staticmethod
+    def validate_user(data):
+        
+        is_valid = True # we assume this is true
+        if not EMAIL_REGEX.match(data['email']):
+            flash("Invalid email address!")
+            is_valid = False
+            
+        print("Validated")
+        return is_valid
