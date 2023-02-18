1dc8e01072b8509aa49655dedec0fdf7d2bcb437
Nicolas Joye <nicolasajoye@gmail.com>
2023-02-18 16:11:11 -0700

    updated

--- Python_Wks_3-7/Assignments/Core/Login and Registration/server.py
+++ Python_Wks_3-7/Assignments/Core/Login and Registration/server.py
@@ -1,8 +1,6 @@
 from flask_app import app
 
 from flask_app.controllers import logins_controller
-from flask_app.controllers import messages_controller
-from flask_app.controllers import comments_controller
 
 if __name__ == "__main__":
     app.run(debug=True)
