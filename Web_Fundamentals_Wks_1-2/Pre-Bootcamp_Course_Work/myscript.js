b771e5c72e7511d642c780dfd9af9a1057d0875a
njoyedevs <118856885+njoyedevs@users.noreply.github.com>
2023-02-21 20:53:03 -0700

    Delete Web_Fundamentals_Wks_1-2 directory

--- Web_Fundamentals_Wks_1-2/Pre-Bootcamp_Course_Work/myscript.js
+++
@@ -1,7 +0,0 @@
-function counter() {    
-    for (var num = 0; num <= 5; num++) {        
-        console.log(num);    
-    }
-}
-counter();    // run the function
-counter();    // run the function again
